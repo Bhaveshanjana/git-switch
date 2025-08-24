889839cb9459cfa0b2c8dadff93b8cfdf0c1e42a
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-24 14:03:43 +0530

    fix some error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,16 @@
 import type { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
       authorization: { params: { scope: "read:user repo" } },
     }),
   ],
   callbacks: {
-    async jwt({ account, profile, token }) {
+    async jwt({ token, account, profile }) {
       if (account) {
         (token as any).accessToken = account.access_token;
       }
