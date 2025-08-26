5f0839c94e91d2ea05c15e5d9272430d50fd2f00
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-08-26 20:52:34 +0530

    update hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { sans, urban } from "@/utils/fonts";
+import { Plus } from "lucide-react";
 import { signIn } from "next-auth/react";
 import React from "react";
 
@@ -10,27 +12,37 @@ const Hero = () => {
         <div className="glow-bg absolute w-[100vw] h-[30vh] top-[-50vh] "></div>{" "}
       </div>
 
-      <div className="relative z-10 mt-44 p-4 w-4xl mx-auto divide-y space-y-3 divide-[#202020]">
-        <h1 className="text-6xl">Manage Your Repos Effortlessly</h1>
-        <p className="text-2xl w-lg mx-auto bg-clip-text text-transparent bg-gradient-to-b from-foreground via-foreground/90 to-muted-foreground">
+      <div className="relative z-10 mt-50 p-4 w-5xl mx-auto space-y-2 border border-slate-200 dark:border-slate-600 [mask-image:radial-gradient(200rem_24rem_at_center,white,transparent)]">
+        <Plus className="absolute -left-4 -top-4 h-8 w-8 text-indigo-500" />
+        <Plus className="absolute -bottom-6 -left-4 h-8 w-8 text-indigo-500" />
+        <Plus className="absolute -right-4 -top-4 h-8 w-8 text-indigo-500" />
+        <Plus className="absolute -bottom-6 -right-4 h-8 w-8 text-indigo-500" />
+        <h1
+          className={`text-6xl bg-clip-text text-transparent bg-gradient-to-b from-[#303020] dark:text-transparent dark:bg-gradient-to-b dark:from-[#2a13d5] dark:to-[#38c71b] ${sans}`}
+        >
+          Manage Your Repos Effortlessly
+        </h1>
+        <p
+          className={`text-2xl w-lg mx-auto bg-clip-text text-gray-600 dark:text-gray-400 ${urban}`}
+        >
           Update repository visibility with just one click.
         </p>
-        <div className="relative">
-          <span>Signup with github</span>
-          <button
-            type="submit"
-            className="absolute right-[50%] -top-0.5 translate-x-30 text-white w-10 h-7 flex items-center justify-center rounded-full bg-white/10 hover:bg-white/20 transition-colors group overflow-hidden"
-          >
-            <span className="relative w-full h-full block overflow-hidden">
-              <span className="absolute inset-0 flex items-center justify-center transition-transform duration-300 group-hover:translate-x-full">
-                →
-              </span>
-              <span className="absolute inset-0 flex items-center justify-center transition-transform duration-300 -translate-x-full group-hover:translate-x-0">
-                →
-              </span>
+        <button
+          type="submit"
+          onClick={(()=> signIn())}
+          className="relative flex items-center justify-center space-x-2 border border-slate-200 dark:border-slate-600  bg-[#20202000] px-4 py-1 mx-auto rounded-lg cursor-pointer"
+        >
+          <span className="text-white">Signup</span>
+
+          <span className="relative w-9 h-9 flex items-center justify-center rounded-full bg-white/10 hover:bg-white/20 transition-colors group overflow-hidden">
+            <span className="absolute inset-0 flex items-center justify-center transition-transform duration-300 group-hover:translate-x-full">
+              →
             </span>
-          </button>
-        </div>
+            <span className="absolute inset-0 flex items-center justify-center transition-transform duration-300 -translate-x-full group-hover:translate-x-0">
+              →
+            </span>
+          </span>
+        </button>
       </div>
     </div>
   );
